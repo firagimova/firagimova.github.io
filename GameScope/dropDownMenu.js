93939bc13a8b435966326c16a6c3368708bdf5d9
Firuza Rahimova <95957162+firagimova@users.noreply.github.com>
2024-05-10 01:10:22 +0300

    sprint 3

---
+++ GameScope/dropDownMenu.js
@@ -0,0 +1,10 @@
+<script>
+$(document).ready(function() {
+    $('ul.sf-menu').superfish({
+        // Superfish options here
+        delay: 800,
+        animation: { opacity: 'show', height: 'show' },
+        speed: 'fast'
+    })
+});
+</script>
---
+++ GameScope/lightBox.js
@@ -0,0 +1,5 @@
+
+$(document).ready(function () {
+    // Initialize all lightbox elements
+    $('[data-lightbox="light-box"]').lightbox()
+})
